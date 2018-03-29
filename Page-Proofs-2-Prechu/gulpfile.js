b1427e8d638a02b5f49a1e519cd0d01944dd82ee
Никита <lesaal02@gmail.com>
2018-03-29 19:56:01 +0300

    non-adaptive version

--- Page-Proofs-2-Prechu/gulpfile.js
+++ Page-Proofs-2-Prechu/gulpfile.js
@@ -1,11 +1,11 @@
-var gulp = require('gulp'),//подключаем gulp
-    sass = require('gulp-sass');//подключаем sass пакет
+var gulp = require('gulp'),//connected gulp
+    sass = require('gulp-sass');//using sass pocket
 gulp.task('sass',function(){
-    return gulp.src(['sass/**/*.sass','sass/**/*.scss'])//берем источник
+    return gulp.src(['sass/**/*.sass','sass/**/*.scss'])
         .pipe(sass({outputStyle:'expanded'}).on('error',sass.logError))
         .pipe(gulp.dest('css'))
 });
 gulp.task('watch',function(){
-    gulp.watch(['sass/**/*.sass','sass/**/*.scss'],['sass']);//наблюдение за sass файлами в папке sass
+    gulp.watch(['sass/**/*.sass','sass/**/*.scss'],['sass']);//watching the files in the css folder
 });
 gulp.task('default', ['watch']);
